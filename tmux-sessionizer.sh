c4a623816cea46bc97ccf31f3d003f1a18ce4b24
Stefan Mack <developer@mack.aleeas.com>
2025-10-19 13:50:18 +0200

    Init

---
+++ tmux-sessionizer.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+	$(find ~/dev -mindepth 3 -maxdepth 3 -type d > ~/.config/tmux/tmux-sessionizer-project-directories)
+	selected=$(cat ~/.config/tmux/tmux-sessionizer-directories ~/.config/tmux/tmux-sessionizer-project-directories | fzf)
+fi
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+if [[ -z $TMUX ]]; then
+	tmux attach -t $selected_name
+	exit 0
+fi
+
+tmux switch-client -t $selected_name
